4a3bf87470d2dc1e4278a7a1354b85168b75306b
liuqipeng <499780990@qq.com>
2022-04-20 16:25:56 +0800

    update

--- lib/dart_core_test.dart
+++ lib/dart_core_test.dart
@@ -1,9 +1,13 @@
 //dart:core是默认导入的
 void main(){
+  //print方法
+  print('my name is potter');
   //parse方法
   assert(int.parse('42') == 42);
   assert(int.parse('0x42') == 66);
   assert(double.parse('0.50') == 0.5);
+  assert(double.parse('1.2e+2') == 120.0);
+  assert(double.parse('1.2e-2') == 0.012);
   //radix是进制
   assert(int.parse('42', radix: 16) == 66);
 }
--- lib/method_test.dart
+++ lib/method_test.dart
@@ -1,11 +1,17 @@
-void main(){
-
+void main() {
+  //age没有制定的时候是null，null也可以toString，打印'null'这个字符串
+  testAlterMethod('potter');
+  testAlterMethod('potter', 30);
 }
 
 /*方法*/
 
-void testMethod() {
+void testMethod() {}
 
+//可选参数,注意要加?
+//加个?就可以了，就是告诉编译器这是nullability类型，告诉编译器如果为空，我后面自己会处理的。
+void testAlterMethod(String name, [int? age, String? home]) {
+  print(name + age.toString());
 }
 
 /*是=>而不是->;
