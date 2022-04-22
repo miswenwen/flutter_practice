31e5dd80b2047c1becf8a8b2397b129f35ebadf7
liuqipeng <499780990@qq.com>
2022-04-22 15:07:22 +0800

    update

--- lib/dart_core_test.dart
+++ lib/dart_core_test.dart
@@ -1,5 +1,5 @@
 //dart:core是默认导入的
-void main(){
+void main() {
   //print方法
   print('my name is potter');
   //parse方法
@@ -10,4 +10,25 @@ void main(){
   assert(double.parse('1.2e-2') == 0.012);
   //radix是进制
   assert(int.parse('42', radix: 16) == 66);
+
+  //字符串相关方法
+  // 检查一个字符串是否包含另一个字符串。
+  assert('Never odd or even'.contains('odd'));
+  // 一个字符串是否以另一个字符串为开头?
+  assert('Never odd or even'.startsWith('Never'));
+  // 一个字符串是否以另一个字符串为结尾?
+  assert('Never odd or even'.endsWith('even'));
+  // 查找一个字符串在另一个字符串中的位置。
+  assert('Never odd or even'.indexOf('odd') == 6);
+
+  //关键字in的一种应用场景
+  for (var char in 'hello'.split('')) {
+    print(char);
+  }
+
+  // Trim a string. trim() 移除首尾空格
+  assert('  hello  '.trim() == 'hello');
+
+  //正则表达式regular expression，记好是RegExp即可，后续用的时候再细看
+  var numbers = RegExp(r'\d+');
 }
--- lib/var_test.dart
+++ lib/var_test.dart
@@ -1,4 +1,4 @@
-import 'dart:core';
+//Number,List,Set,Map
 
 void main() {
   testVar();
@@ -14,17 +14,44 @@ void testVar() {
 
   //set
   var set = {'a', 'b', 'c'};
+  var set2 = Set<String>();
+  set2.add('a');
+  set2.add('b');
 
   //list,注意变量名的格式 和java的是一致的
   var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
   //下面这种写法编译不过
   // The default 'List' constructor isn't available when null safety is enabled. (Documentation)
+  //可以参考https://dart.dev/null-safety/understanding-null-safety#no-unnamed-list-constructor,understanding null safety
   //var list2 = List<String>();
   var list2 = <String>[];
   list2.add('a1');
 
   //map
   var testMaps = {'name': 'potter', 'age': '30', 'home': 'yichang'};
+  var map2 = Map<String, String>();
+  //注意赋值方式，并不是add('name','potter');
+  map2['name'] = 'potter';
+  map2['age'] = '30';
+
+  //Iterable
+  var coffees = [];
+  var teas = ['green', 'black', 'chamomile', 'earl grey'];
+  //isEmpty&isNotEmpty
+  assert(coffees.isEmpty);
+  assert(teas.isNotEmpty);
+
+  //for each
+  var teas2 = ['green', 'black', 'chamomile', 'earl grey'];
+  teas2.forEach((tea) => print('I drink $tea'));
+
+  //where(),any(),every()
+  var teas3 = ['green', 'black', 'chamomile', 'earl grey'];
+  bool isDecaffeinated(String teaName) => teaName == 'chamomile';
+  // 使用 where() 来查找元素，这些元素在给定的函数中返回 true 。
+  var decaffeinatedTeas = teas3.where((tea) => isDecaffeinated(tea));
+  // 使用 any() 来检查集合中是否至少有一个元素满足条件。
+  assert(teas3.any(isDecaffeinated));
+  // 使用 every() 来检查集合中是否所有元素满足条件。
+  assert(!teas3.every(isDecaffeinated));
 }
-
-
