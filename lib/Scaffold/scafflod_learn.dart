092277d93c90c04b60a9826c181035cafa905aac
liuqipeng <499780990@qq.com>
2022-04-27 16:39:48 +0800

    scaffold update

--- lib/Scaffold/scafflod_learn.dart
+++ lib/Scaffold/scafflod_learn.dart
@@ -10,20 +10,63 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: HomePage(),
+      home: ScaffoldRoute(),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class ScaffoldRoute extends StatefulWidget {
+  @override
+  State<ScaffoldRoute> createState() {
+    return _ScaffoldRouteState();
+  }
+}
+
+class _ScaffoldRouteState extends State<ScaffoldRoute> {
+  //注意第一个item的index是0
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text('learn'),
+        title: Text('App Name'),
+        actions: <Widget>[
+          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: Icon(Icons.share)),
+        ],
       ),
-      body: Text('text'),
+      //drawer: MyDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.business), label: 'Business'),
+          BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+        ],
+        currentIndex: _selectedIndex,
+        fixedColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
+      body: Text('text $_selectedIndex'),
     );
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+}
+
+class MyDrawer extends StatelessWidget {
+  //这行不写会怎么样？就用默认的构造方法
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
   }
 }
