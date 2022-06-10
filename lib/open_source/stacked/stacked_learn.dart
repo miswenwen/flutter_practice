c053fd028358dc3b1aec41f1b84586eb01df7f95
liuqipeng <499780990@qq.com>
2022-06-10 16:13:40 +0800

    update

--- lib/open_source/stacked/stacked_learn.dart
+++ lib/open_source/stacked/stacked_learn.dart
@@ -36,13 +36,17 @@ class HomeViewMultipleWidgets extends StatelessWidget {
           ),
 
           ///这里测试reactive 和nonReactive
-          ///实测无论对于reactive，它里面包的所有view都会刷新一次
-          ///对于nonReactive，它里面包的只有继承于ViewModelWidget的才会刷新
+          ///实测无论对于reactive，它的builder里面包的所有view都会刷新一次
+          ///对于nonReactive，它的builder里面包的只有继承于ViewModelWidget的才会刷新
           ///对于两者来说，外面没包住的ui的不会刷新，例如上面的checkbox
+          ///整个布局用reactive包起来，但是少调用notifyListeners，因为是全局刷新
+          ///然后嵌套一些nonreactive,不同的ViewModelBuilder<XXXModel>.nonReactive去实现局部刷新
           ViewModelBuilder<HomeViewModel>.reactive(
             viewModelBuilder: () => HomeViewModel(),
             onModelReady: (viewModel) => viewModel.initialise(),
+            disposeViewModel: false,
             builder: (context, viewModel, _) {
+              print('builder');
               return Column(
                 children: <Widget>[
                   FloatingActionButton(
@@ -107,6 +111,7 @@ class HomeViewModel extends ChangeNotifier {
   String title = 'default';
 
   void initialise() {
+    print('HomeViewModel initialise');
     title = 'initialised';
     notifyListeners();
   }
