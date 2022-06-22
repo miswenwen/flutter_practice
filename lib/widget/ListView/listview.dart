668fd7706675b26f90effe69d4df9b28258b2ec1
liuqipeng <499780990@qq.com>
2022-06-22 11:27:05 +0800

    update

--- lib/widget/ListView/listview.dart
+++ lib/widget/ListView/listview.dart
@@ -7,8 +7,8 @@ import 'package:flutter/material.dart';
  */
 
 ///常见需求：
-///ListView填满剩余控件
-///ListView固定若干个Item高度
+///ListView填满剩余控件 done
+///ListView固定若干个Item高度  done
 ///ListView加分隔线
 ///ListView分Type加载
 ///ListViewItem里的Checkbox，progressbar状态维护
@@ -29,7 +29,7 @@ class _ListViewLearnState extends State<ListViewLearn> {
       body: Column(
         children: [
           Text('1111111'),
-          unchangedItemHeightList3(),
+          listWithDivider2(),
         ],
       ),
     );
@@ -48,7 +48,8 @@ class _ListViewLearnState extends State<ListViewLearn> {
     );
   }
 
-  ///ListView固定若干个Item高度,利用itemExtent，注意有overScroll效果,常用
+  ///ListView固定若干个Item高度,利用itemExtent和shrinwrap，注意有overScroll效果,常用
+  ///注意这里shrinwrap是要配合用的，因为我们毕竟设置的是item高度嘛
   Widget unchangedItemHeightList1() {
     return Container(
       //color: Colors.blue,
@@ -68,6 +69,7 @@ class _ListViewLearnState extends State<ListViewLearn> {
 
   ///ListView固定若干个Item高度,利用shrinkWrap = true，常用
   ///shrinkwrap，以该例来说，shrinkWrap = true,container的高度就可以不用设置
+  ///shrinkwrap默认值是false，意思就是用item的高度来生成listview的高度
   Widget unchangedItemHeightList2() {
     return ListView.builder(
       shrinkWrap: true,
@@ -98,6 +100,57 @@ class _ListViewLearnState extends State<ListViewLearn> {
         },
         itemCount: 3,
       ),
+    );
+  }
+
+  ///ListView加分隔线,item的底部加一条线
+  Widget listWithDivider1() {
+    return ListView.builder(
+      itemCount: 3,
+      itemExtent: 50,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Stack(
+          children: [
+            Center(
+              child: Text('aaa $index'),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Divider(
+                height: 1,
+                thickness: 1,
+                color: Colors.yellow,
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  ///ListView加分隔线,使用ListView.separated来做的，不太推荐
+  ///但是最后一个item的后面是没有分隔线的。得去外面加，或者itemBuilder里面判断index是最后一个，加个分隔线。
+  ///这里就没有itemExtent这个属性了。shrinkWrap依然可以用。
+  Widget listWithDivider2() {
+    return ListView.separated(
+      itemCount: 3,
+      shrinkWrap: true,
+      itemBuilder: (context, index) {
+        return Container(
+          height: 50,
+          child: Center(
+            child: Text('aaa $index'),
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        return Divider(
+          height: 1,
+          thickness: 1,
+          color: Colors.yellow,
+        );
+      },
     );
   }
 }
